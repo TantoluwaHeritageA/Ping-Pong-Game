0228c453b934c77206505049eb3ca49bf6debd43
TantoluwaHeritageA <heritage1609@gmail.com>
2022-07-19 15:12:28 +0300

    pong game

---
+++ withoutoop.py
@@ -0,0 +1,23 @@
+# from turtle import Screen , Turtle
+# screen = Screen()
+# screen.setup(width=800 , height=600)
+# screen.bgcolor("black")
+# screen.title("Pong Game")
+# screen.tracer(0)
+
+# paddle = Turtle()
+# paddle.shape("square")
+# paddle.color("white")
+# paddle.shapesize(stretch_wid=5 , stretch_len=1)
+# paddle.penup()
+# paddle.setpos(350 , 0)
+#
+#
+# def go_up():
+#     new_y = paddle.ycor() + 20
+#     paddle.goto(paddle.xcor() , new_y)
+#
+#
+# def go_down():
+#     new_y = paddle.ycor() - 20
+#     paddle.goto(paddle.xcor() , new_y)
